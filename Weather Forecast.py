86608462b018150199985279b87967fcf5cfb5fa
lleight1 <lleight1@students.kennesaw.edu>
2024-04-22 21:20:10 -0400

    Created Weather Forecast.py
    Added initial code

---
+++ Weather Forecast.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import requests
+import json
+import datetime
+from PIL import ImageTk, Image
+
+main = Tk()
+main.title("Weather Forecast")
+main.geometry("450x700")
+main['background'] = "white"
+
+main.mainloop()
